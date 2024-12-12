527082bb14f13e6e1cd6b0e474e5e84f696aaeed
Vu.PM <Vu.PM@iigvietnam.edu.vn>
2024-12-12 09:39:28 +0700

    ud

--- routes/zaloProduct.js
+++ routes/zaloProduct.js
@@ -1,14 +1,15 @@
 const express = require("express");
 const { addProduct, editProduct, deleteProduct, addManyProducts, deleteManyProducts, changeProductStatus, getAllProducts } = require("../controllers/zaloProducts");
+const isAuth = require("../middleware/isAuth");
 
 const router = express.Router();
 
-router.post('/customer/zaloproducts', addProduct);
-router.put('/customer/zaloproducts/:id', editProduct);
-router.delete('/customer/zaloproducts/:id', deleteProduct);
-router.post('/customer/zaloproducts/many', addManyProducts);
-router.delete('/customer/zaloproducts/many', deleteManyProducts);
-router.put('/customer/zaloproducts/status/:id', changeProductStatus);
-router.get('/customer/zaloproducts', getAllProducts);
+router.post('/customer/zaloproducts', isAuth, addProduct);
+router.put('/customer/zaloproducts/:id', isAuth, editProduct);
+router.delete('/customer/zaloproducts/:id', isAuth, deleteProduct);
+router.post('/customer/zaloproducts/many', isAuth, addManyProducts);
+router.delete('/customer/zaloproducts/many', isAuth,deleteManyProducts);
+router.put('/customer/zaloproducts/status/:id', isAuth, changeProductStatus);
+router.get('/customer/zaloproducts', isAuth, getAllProducts);
 
 module.exports = router;
