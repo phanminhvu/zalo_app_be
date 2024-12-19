94c33856893372baf25be5d15ab9f5eaa4bd5a97
Tùng <tungxuan101998@gmail.com>
2024-12-19 22:33:02 +0700

    feat: form-data add and edit product

--- routes/zaloProduct.js
+++ routes/zaloProduct.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const { addProduct, editProduct, deleteProduct, addManyProducts, deleteManyProducts, changeProductStatus, getAllProducts } = require("../controllers/zaloProducts");
 const isAuth = require("../middleware/isAuth");
+const { uploadDisk } = require("../utils/multer.js")
 
 const router = express.Router();
 
-router.post('/customer/zaloproducts', isAuth, addProduct);
-router.put('/customer/zaloproducts/:id', isAuth, editProduct);
+router.post('/customer/zaloproducts', isAuth, uploadDisk.single('image'), addProduct);
+router.put('/customer/zaloproducts/:id', isAuth, uploadDisk.single('image'), editProduct);
 router.delete('/customer/zaloproducts/:id', isAuth, deleteProduct);
 router.post('/customer/zaloproducts/many', isAuth, addManyProducts);
 router.delete('/customer/zaloproducts/many', isAuth,deleteManyProducts);
