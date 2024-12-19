613f718412389bf56e0916dd284568d3ac82f778
Vu.Pm <vu.pm@iigvietnam.edu.vn>
2024-12-19 20:17:54 +0700

    ud

--- models/zaloproducts.js
+++ models/zaloproducts.js
@@ -3,10 +3,6 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-    id: {
-        type: Number,
-        required: true,
-    },
     name: {
         type: String,
         required: true,
